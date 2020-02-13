754123c9c491686f2b2e0a7ee8f3e497d6817e29
alfieqashwa <alfieqashwa@gmail.com>
2020-02-13 19:57:52 +0700

    [useReducer && Custom Consumer Hooks]

--- src/index.js
+++ src/index.js
@@ -1,41 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
 
-import Counter from "./components/Counter";
+import App from "./App";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-const initialState = { count: 42 };
-
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case "INCREMENT":
-      return {
-        count: state.count + 1
-      };
-    case "DECREMENT":
-      return {
-        count: state.count - 1
-      };
-    case "RESET":
-      return {
-        count: state.count * 0
-      };
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Counter />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
